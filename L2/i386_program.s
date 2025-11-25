b50fb44361781c45aa90297f01c3e0d445fe0abe
Salvatore Fadda <37809998+SalvatoreFadda@users.noreply.github.com>
2025-11-25 15:28:37 +0100

    Refactor comments to single-line style
    Updated comments to use single-line style for clarity.

--- L2/i386_program.s
+++ L2/i386_program.s
@@ -4,18 +4,18 @@ _b$ = 12                                        ; size = 4
 _sum    PROC
         push    ebp
         mov     ebp, esp
-        mov     eax, DWORD PTR _a$[ebp]         ## move the word in ebp + a into the eax register
-        add     eax, DWORD PTR _b$[ebp]         ## sums to eax the word that is in ebp + b
+        mov     eax, DWORD PTR _a$[ebp]         ; move the word in ebp + a into the eax register
+        add     eax, DWORD PTR _b$[ebp]         ; sums to eax the word that is in ebp + b
         pop     ebp
-        ret     0                               ## concludes and consumes the PC register - goes to line 19
+        ret     0                               ; concludes and consumes the PC register - goes to line 19
 _sum    ENDP
 
 _main   PROC
         push    ebp
-        mov     ebp, esp                        ## the mov copies the register on the right to the one on the left
+        mov     ebp, esp                        ; the mov copies the register on the right to the one on the left
         push    20                              ; 00000014H
         push    10                              ; 0000000aH
-        call    ebp                             # jump to line 4 and push the PC register
+        call    ebp                             ; jump to line 4 and push the PC register
         add     esp, 8
         pop     ebp
         ret     0
