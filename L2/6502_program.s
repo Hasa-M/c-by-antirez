353290e2a428032ea284d43835b26c15296784c5
Salvatore Fadda <37809998+SalvatoreFadda@users.noreply.github.com>
2025-11-25 15:28:22 +0100

    Refactor comments in 6502_program.s
    Updated comments to use semicolons instead of double hashes for consistency.

--- L2/6502_program.s
+++ L2/6502_program.s
@@ -1,19 +1,19 @@
-## print video 50 green pixels in sequence
+; print video 50 green pixels in sequence
 
 START:
-    LDA #50         ## load the value 50 into register A
-    JSR FILL_FIVE   ## after loading the value jump to the FILL_FIVE function
+    LDA #50         ; load the value 50 into register A
+    JSR FILL_FIVE   ; after loading the value jump to the FILL_FIVE function
     BRK
 
-FILL_FIVE:          ## it starts at 200 ($0200) and writes to memory 50 times 5 moving sequentially
-    TAX             ## transfer A to X
-    BEQ DONE        ## if it is 0, the instruction is completed, it goes to line 18. Case in which LDA #N has N = 0
-    LDY #$00        ## load the value 0 into the Y register
-    LDA #$05        ## load the value 5 into the X register
+FILL_FIVE:          ; it starts at 200 ($0200) and writes to memory 50 times 5 moving sequentially
+    TAX             ; transfer A to X
+    BEQ DONE        ; if it is 0, the instruction is completed, it goes to line 18. Case in which LDA #N has N = 0
+    LDY #$00        ; load the value 0 into the Y register
+    LDA #$05        ; load the value 5 into the X register
 LOOP:
-    STA $0200, Y    ## records the value of A at location -> base address + value of Y, which then declares the offset
-    INY             ## increment by one unit Y
-    DEX             ## decrease X by one unit
-    BNE LOOP        ## if X is not zero jump to LOOP
+    STA $0200, Y    ; records the value of A at location -> base address + value of Y, which then declares the offset
+    INY             ; increment by one unit Y
+    DEX             ; decrease X by one unit
+    BNE LOOP        ; if X is not zero jump to LOOP
 DONE:
     RTS
